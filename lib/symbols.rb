926d4ce37b09d1cbfd318372856402ec20026cec
kenyounot123 <kenlu519@gmail.com>
2023-08-29 16:39:04 -0400

    Organize code into modules

---
+++ lib/symbols.rb
@@ -0,0 +1,41 @@
+#Class for symbols, unicode
+class Symbols 
+  def white_king
+    "\u2654"
+  end
+  def white_queen
+    "\u2655"
+  end
+  def white_rook
+    "\u2656"
+  end
+  def white_bishop 
+    "\u2657"
+  end
+  def white_knight
+    "\u2658"
+  end
+  def white_pawn
+    "\u2659"
+  end
+
+  def black_king
+    "\u265A"
+  end
+  def black_queen
+    "\u265B"
+  end
+  def black_rook
+    "\u265C"
+  end
+  def black_bishop 
+    "\u265D"
+  end
+  def black_knight
+    "\u265E"
+  end
+  def black_pawn
+    "\u265F"
+  end
+  
+end
