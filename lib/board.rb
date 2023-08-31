30d7de077bb064376c8a5d4e5741ca002c1a150f
kenyounot123 <kenlu519@gmail.com>
2023-08-31 16:24:07 -0400

    update #print_rows from Board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,22 +3,32 @@ module Displayable
   private
   def print_chess_board
     system 'clear'
-    puts "\e[32m   a b c d e f g h\e[0m"
-    print_board
-    puts "\e[32m   a b c d e f g h\e[0m"
+    puts "\e[32m    a  b  c  d  e  f  g  h\e[0m"
+    print_board 
+    puts "\e[32m    a  b  c  d  e  f  g  h\e[0m"
   end
 
   def print_board
     @grid.each_with_index do |row, index|
       print "\e[32m #{8 - index} \e[0m"
-      print "#{row[0]}ddddddddddddd"
+      print_rows(row, index)
       puts "\e[32m #{8 - index} \e[0m"
     end
   end
 
-  def print_rows(row, index)
-    puts "#{@grid[row][index]}"
+  def print_rows(row_array, row_index)
+    row_array.each_with_index do |square, index|
+      if row_index % 2 == 0 #even index  
+        print square if index % 2 == 0 #even squares are white
+        print square if index % 2 == 1 # odd squares are black
+      elsif row_index % 2 == 1 
+        print square if index % 2 == 0 #even squares are black
+        print square if index % 2 == 1 # odd squares are white
+      end
+    end
   end
+  #if row index = odd do white square then black square
+  #if row index = even do black square then white square
 
 
 end
@@ -27,7 +37,7 @@ class Board
   include Displayable
   attr_accessor :grid
   def initialize()
-    @grid = Array.new(8) { Array.new(8, 'hi') }
+    @grid = Array.new(8) { Array.new(8) }
     print_chess_board
   end
 
